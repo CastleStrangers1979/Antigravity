60ade31aaac815900c339c135b02c728c0e93506
Z User <z@container>
2026-05-29 10:10:16 +0000

    4d7c3935-7b2d-44eb-9236-acd608e0d16b

--- keepalive.sh
+++ keepalive.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-cd /home/z/my-project/.next/standalone
+cd /home/z/my-project
 while true; do
-    if ! pgrep -f "node server.js" > /dev/null; then
-        echo "[$(date)] Starting server..." >> /home/z/my-project/keepalive.log
-        node server.js >> /home/z/my-project/keepalive.log 2>&1 &
-        sleep 5
-    fi
-    sleep 10
+    bun run dev &
+    PID=$!
+    wait $PID
+    echo "Server died, restarting..." >> /home/z/my-project/dev.log
+    sleep 2
 done
